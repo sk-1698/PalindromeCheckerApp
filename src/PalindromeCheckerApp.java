dfb2c06f5f87d7d437c867ae6d7f6fc3e52085f4
sk-1698 <sk1698@srmist.edu.in>
2026-02-24 15:29:00 +0530

    uc3 error fixed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        public static void main(String[] args) {
+
 
             // Original string
             String original = "level";
