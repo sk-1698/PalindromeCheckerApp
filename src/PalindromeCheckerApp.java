6a9258fbf6c3f557890f6f2240f672addcd6f81e
sk-1698 <sk1698@srmist.edu.in>
2026-02-24 15:34:24 +0530

    error fixed in uc6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
         String original = "level";
