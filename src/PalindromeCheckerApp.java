b5064a3a43ed0e37441e185dfc70ff6d6376cf7c
sk-1698 <sk1698@srmist.edu.in>
2026-02-24 15:31:00 +0530

    uc4 error fixed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        public static void main(String[] args) {
+
 
             // Original string
             String original = "radar";
